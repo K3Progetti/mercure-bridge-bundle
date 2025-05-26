b797a1919a0196632b6a3fe550f958c6f25926f2
Mattia Vitali <vitali.mattia@gmail.com>
2025-05-26 14:31:09 +0200

    fix: name operation

--- src/Enum/NotificationOperation.php
+++ src/Enum/NotificationOperation.php
@@ -5,18 +5,20 @@ namespace K3Progetti\MercureBridgeBundle\Enum;
 enum NotificationOperation: string
 {
     case DownloadReady = 'download_ready';
-    case CommandStarted = 'command_started';
-    case CommandCompleted = 'command_completed';
-    case CommandFailed = 'command_failed';
+    case DownloadStarted = 'download_started';
+    case DownloadCompleted = 'download_completed';
+    case DownloadFailed = 'download_failed';
+    case DownloadProgress = 'download_progress';
 
     // Potresti anche usare metodi helper
     public function description(): string
     {
         return match($this) {
             self::DownloadReady => 'Il file è pronto per il download',
-            self::CommandStarted => 'Comando avviato',
-            self::CommandCompleted => 'Comando completato con successo',
-            self::CommandFailed => 'Comando fallito',
+            self::DownloadStarted => 'Comando avviato',
+            self::DownloadCompleted => 'Comando completato con successo',
+            self::DownloadFailed => 'Comando fallito',
+            self::DownloadProgress => 'Download in elaborazione',
         };
     }
 }
